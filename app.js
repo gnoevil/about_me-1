5c4a3e930664864802d7a4170f6fd01ffe7770e4
Nikko Pisciotti <t.bird.13@hotmail.com>
2016-10-19 22:00:49 -0700

    restructured all of my script to operate within a 'for' loop. Also created scenarios in which users didn't cooperate.

--- app.js
+++ app.js
@@ -1,55 +1,102 @@
 'use strict';
 
-var welcome = alert('Welcome to my site! We will be playing a guessing game that builds on itself!');
-console.log('Welcome to my site! We will be playing a guessing game that builds on itself!');
+var guitarsOwned = ['les paul', 'stratocaster', 'dot'];
+var myFaveNum = Math.floor(Math.random() * 30);   //myFaveNum will be a whole number between 1 and 29//
 
-var userName = prompt('First question! What is your name?');
-console.log('First question! What is your name?');
+alert('Welcome to my site! We will be playing a small game that builds on itself!');
+console.log('Welcome to my site! We will be playing a small game that builds on itself!');
 
-if (userName === null) {
-  var userName = 'n00b';
+for (var i = 0; i < 7; i++) {
+  if (i === 0) {
+    var userName = prompt('First question! What is your name?');
+    console.log('First question! What is your name?');
+
+    if (userName === null) {
+      var userName = 'n00b';
+    }
+
+    alert('Your name is ' + userName + '? Pleasure to meet you, my name is Nikko!');
+    console.log('Your name is ' + userName + '? Pleasure to meet you, my name is Nikko!');
+
+  } else if (i === 1) {
+    var q2 = prompt('Ok ' + userName + ', I\'m going to ask you some questions. First question, how old are you?');
+    console.log('Ok ' + userName + 'I\'m going to ask you some questions. First question, how old are you?');
+    console.log(parseInt(q2));
+  } else if (i === 2) {
+    var q3 = prompt('Thanks for playing along! Next question: What\'s your favorite color?');
+    console.log('Thanks for playing along! Next question: What\'s your favorite color?');
+    console.log(q3);
+  } else if (i === 3) {
+    var q4 = prompt('How cool! Ok ' + userName + ', tell me your favorite hobby.');
+    console.log('How cool! Ok ' + userName + ', tell me your favorite hobby.');
+    console.log(q4);
+  } else if (i === 4) {
+    var q5 = prompt('Alright! Thanks for playing along thus far, ' + userName + '! In a moment you\'ll have a chance to guess something about me! But I have one more for you first. Name the city you live in!');
+    console.log('Alright! Thanks for playing along thus far, ' + userName + '! In a moment you\'ll have a chance to guess something about me! But I have one more for you first. Name the city you live in!');
+    console.log(q5);
+  } else if (i === 5) {
+    alert('Ok, now it\'s time for a guessing game! I want you to try and guess the number I\'m thinking of! Give it a go, but you only get four chances!');
+    console.log('Ok, now it\'s time for a guessing game! I want you to try and guess the number I\'m thinking of! Give it a go, but you only get four chances!');
+    for (var j = 1; j < 5; j++) {
+      var q6 = prompt('What\'s my favorite number?');
+      console.log('What\'s my favorite number?');
+      console.log(q6);
+      var userNumGuess = parseInt(q6);
+      if (userNumGuess === myFaveNum) {
+        alert('Nice guess ' + userName + '! One more question!');
+        console.log('Nice guess ' + userName + '! One more question!');
+        guessCorrect = true;
+        break;
+      } else if (userNumGuess < myFaveNum) {
+        alert('Oops! My favorite number is higher than your guess, try again!');
+        console.log('Oops! My favorite number is higher than your guess, try again!');
+        console.log(q6);
+      } else if (userNumGuess > myFaveNum) {
+        alert('Oops! My favorite number is lower than your guess, try again!');
+        console.log('Oops! My favorite number is lower than your guess, try again!');
+        console.log(q6);
+        var guessCorrect = false;
+      } else if (isNaN(userNumGuess)) {
+        alert('Hey! That\'s not a number, but that counts as a guess!');
+        console.log('Hey! That\'s not a number, but that counts as a guess!');
+        var guessCorrect = false;
+      }
+    }
+    var numTries = j;
+    if (guessCorrect === false) {
+      alert('Uh oh! You didn\'t get the correct answer within 4 tries, it was ' + myFaveNum + '. Try the game over!');
+      console.log('Uh oh! You didn\'t get the correct answer within 4 tries, it was ' + myFaveNum + '. Try the game over!');
+      break;
+    }
+  } else if (i === 6) {
+    for (var k = 1; k < 7; k++) {
+      var q7 = prompt('Last question! I LOVE guitars! Can you guess what kind of guitar I have? I\'ll give you a hint, I have 3 guitars. The correct answer is a famous model of FENDER, GIBSON, or EPIPHONE. I only need the model name. Six attempts!');
+      console.log('Last question! I LOVE guitars! Can you guess what kind of guitar I have? I\'ll give you a hint, I have 3 guitars. The correct answer is a famous model of FENDER, GIBSON, or EPIPHONE. I only need the model name. Six attempts!');
+      if (isNaN(q7) && q7 !== null) {
+        if (q7.toLowerCase() === guitarsOwned[0] || q7.toLowerCase() === guitarsOwned[1] || q7.toLowerCase() === guitarsOwned[2]) {
+          alert('Wow! You did it ' + userName + '! Thanks for playing along!');
+          console.log('Wow! You did it ' + userName + '! Thanks for playing along!');
+          var guessCorrect2 = true;
+          break;
+        } else if (q7.toLowerCase() !== guitarsOwned[0] || q7.toLowerCase() !== guitarsOwned[1] || q7.toLowerCase() !== guitarsOwned[2]) {
+          alert('Oops! That\'s either a guitar that I don\'t own or it\'s spelled incorrectly, try again!');
+          console.log('Oops! That\'s either a guitar that I don\'t own or it\'s spelled incorrectly, try again!');
+        }
+      } else {
+        alert('You should be inputting only letters, this is case insensitive!');
+        console.log('You should be inputting only letters, this is case insensitive!');
+      }
+    }
+    var numTries2 = k;
+    if (guessCorrect2 === false) {
+      alert('Bummer! Sorry ' + userName + ', you didn\'t get the correct model within 6 tries, maybe play again?');
+      console.log('Bummer! Sorry ' + userName + ', you didn\'t get the correct model within 6 tries, maybe play again?');
+      break;
+    }
+  }
 }
 
-console.log(userName);
-
-alert('Your name is ' + userName + '? Pleasure to meet you, my name is Nikko!');
-console.log('Your name is ' + userName + '? Pleasure to meet you, my name is Nikko!');
-
-var question2 = prompt('Ok, ' + userName + ' the rest of our questions will be in a YES or NO format. Are you cool with that? (Just type YES or NO, this is not case sensitive!)').toUpperCase();
-console.log('Ok, ' + userName + ' the rest of our questions will be in a YES or NO format. Are you cool with that? (select OK for YES) (Just type YES or NO, this is not case sensitive!)');
-console.log(question2);
-
-if (question2 === 'YES') {
-  var question3 = prompt('Thanks for playing along! Next question: Are you interested in coding?').toUpperCase();
-  console.log('Thanks for playing along! Next question: Are you interested in coding? Remember, OK means YES!');
-  console.log(question3);
-} else if (question2 === 'NO') {
-  alert('Well my friend, you don\'t have to play!');
-  console.log('Well my friend, you don\'t have to play!');
-}
-
-if (question3 === 'YES') {
-  var question4 = prompt('Awesome, me too! I\'ve learned quite a bit about you ' + userName + '! Are you ready for one more question? If so, type YES!').toUpperCase();
-  console.log('Awesome, me too! I\'ve learned quite a bit about you ' + userName + '! Are you ready for one more question? If so, type YES!');
-  console.log(question4);
-} else if (question3 === 'NO') {
-  alert('Well maybe you should give coding a try! Try again =)');
-  console.log('Well maybe you should give coding a try! Try again =)');
-}
-
-if (question4 === 'YES') {
-  var question5 = prompt('Ok! Now that we\'ve been acquainted and know we have similar interests, do you think we can be friends ' + userName + '? YES or NO?').toUpperCase();
-  console.log('Ok! Now that we\'ve been acquainted and know we have similar interests, do you think we can be friends ' + userName + '?');
-  console.log(question5);
-} else if (question4 === 'NO') {
-  alert('Well either way, thank you for taking the time to visit my site. Take care ' + userName + '!');
-  console.log('Well either way, thank you for taking the time to visit my site. Take care ' + userName + '!');
-}
-
-if (question5 === 'YES') {
-  alert('Thanks for playing along! I\'ve left some fun facts about me on this site for you to read about. Take care ' + userName + '!');
-  console.log('Thanks for playing along! I\'ve left some fun facts about me on this site for you to read about. Take care ' + userName + '!');
-} else if (question5 === 'NO') {
-  alert('Well either way, thank you for taking the time to visit my site. Take care ' + userName + '!');
-  console.log('Well either way, thank you for taking the time to visit my site. Take care ' + userName + '!');
+if (guessCorrect2 === true) {
+  alert('Ok ' + userName + ', we\'ve made it to the end! Here\'s what you\'ve told me so far. You are: ' + parseInt(q2) + ' years old, your favorite color is: ' + q3 + ', your favorite hobby is: ' + q4 + ', and the city you live in is: ' + q5 + '. Also you answered the first guessing game in: ' + j + ' tries. In the second game you answered correctly in ' + k + ' tries. Oh, what fun it\'s been!');
+  console.log('Ok ' + userName + ', we\'ve made it to the end! Here\'s what you\'ve told me so far. You are: ' + parseInt(q2) + ' years old, your favorite color is: ' + q3 + ', your favorite hobby is: ' + q4 + ', and the city you live in is: ' + q5 + '. Also you answered the first guessing game in: ' + j + ' tries. In the second game you answered correctly in ' + k + ' tries. Oh, what fun it\'s been!');
 }
